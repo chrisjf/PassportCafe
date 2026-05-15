2836c005ea779448c9f95fc57b757a8d9aa15066
Chris <chrisjf@gmail.com>
2026-05-15 11:49:53 -0400

    Initial Commit

---
+++ Passport Cafe/Passport_CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Passport_CafeApp.swift
+//  Passport Cafe
+//
+//  Created by Chris Forbes on 2026-05-15.
+//
+
+import SwiftUI
+
+@main
+struct Passport_CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Passport CafeTests/Passport_CafeTests.swift
@@ -0,0 +1,16 @@
+//
+//  Passport_CafeTests.swift
+//  Passport CafeTests
+//
+//  Created by Chris Forbes on 2026-05-15.
+//
+
+import Testing
+
+struct Passport_CafeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
